2999724fad4fca1cdee7276cfdadcdb95c82ef3d
Ricardo <00200618@uca.edu.sv>
2020-06-13 21:25:55 -0600

    EPA: Aquilesprogramo, 00200618, 00214917, 00085518

---
+++ Projecto Arkanoid/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projecto_Arkanoid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OptionUser optionUser = new OptionUser();
+            optionUser.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Hay que agregar el botton 2
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Projecto Arkanoid/OptionUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Projecto_Arkanoid
+{
+    public partial class OptionUser : Form
+    {
+        public OptionUser()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //agregar register
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //agregar login
+        }
+    }
+}
